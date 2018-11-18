1b31f50a3b27501192984ffccd2af3da22bb0a3b
Grace Michnovicz <gmich@bu.edu>
2018-11-18 15:21:22 -0500

    adding info windows for each marker, formatting

--- BostonDevelopment/wwwroot/js/BostonMarkers.js
+++ BostonDevelopment/wwwroot/js/BostonMarkers.js
@@ -1,5 +1,6 @@
 ﻿
 var map;
+var infowindow;
 function initMap() {
   var boston ={lat: 42.349179, lng: -71.106476};
 
@@ -12,9 +13,7 @@ function initMap() {
 
   map = new google.maps.Map(document.getElementById('map'),
    mapOptions);
-  
-
-  $.getJSON("/api/values", function(json) {
+    $.getJSON("/api/values", function(json) {
 
       $.each(json, function(key, data) {
           var lt = data.lat;
@@ -23,18 +22,29 @@ function initMap() {
                 position: {lat:lt,lng:long}
           });
 
-          var infowindow = new google.maps.InfoWindow();
-
+          //var infowindow = new google.maps.InfoWindow();
+          infowindow = new google.maps.InfoWindow();
           google.maps.event.addListener(marker, 'click', (function(marker, key) {
             return function() {
+              var x = 'OK'
 
               var contentString = '<div id="content">'+
             '<div id="siteNotice">'+
             '</div>'+
-            '<h1 id="firstHeading" class="firstHeading"></h1>'+
-            '<div id="bodyContent">'+
-            '<p><b></b></p>'+
 
+            '<h1 id="firstHeading" class="firstHeading" align="center">'+data.address+'</h1>'+
+
+            '<div id="bodyContent">'+
+            
+            '<img src="http://www.bostonplans.org'+data.imgSrc+'" width="300" height="300" class="center">'+
+            '<p></p>'+
+            '<p style="font-size:160%;"><b>Project Status: </b>'+ data.projectStatus+'</p>'+
+            
+            '<p style="font-size:160%;"><b>Neighborhood: </b>'+data.neighborhood+'</p>'+
+            '<p style="font-size:160%;"><b>Land Square Feet: </b>'+data.landSqFt+'</p>'+
+            '<p style="font-size:160%;"><b>Build Size: </b>'+data.buildSize+'</p>'+
+            '<p style="font-size:160%;"> <a href="'+data.planSrc+'">More information here.</a>'+
+            
             '</div>'+
             '</div>';
 
